b9ff2ffb4458c2bd5ada5f2e2c8e45501ab75a72
Frank Lam <frank@duckpun.ch>
2015-09-09 03:23:16 -0700

    adding api stubs

--- src/utils.js
+++ src/utils.js
@@ -8,11 +8,39 @@ const WHITE = 'white';
 
 export default {
     BLACK, WHITE,
+
     emptyBoard: function(size) {
         if (!isNumber(size) || size <= 0 || size !== parseInt(size)) {
             throw 'An empty board must be created from a positive integer.';
         }
 
         return hashMap('size', size);
+    },
+
+    addBlackMove: function(board, position) {
+    },
+
+    addWhiteMove: function(board, position) {
+    },
+
+    addBlackMoves: function(board, positions) {
+    },
+
+    addWhiteMoves: function(board, positions) {
+    },
+
+    removeMoves: function(board, positions) {
+    },
+
+    equivalentBoards: function(board, positions) {
+    },
+
+    adjacent: function(board, position) {
+    },
+
+    illegalMoves: function(board) {
+    },
+
+    isValidPosition: function(size, position) {
     },
 }
