92fcc168a75e8361a5434bd73eebfe4c535c8523
Frank Lam <frank@duckpun.ch>
2015-09-11 10:15:57 -0700

    cleaning up some api stuff

--- src/utils.js
+++ src/utils.js
@@ -1,46 +1,72 @@
 import {isNumber} from 'lodash';
-import {hashMap} from 'mori';
+import mori from 'mori';
 
 
-const BLACK = 'black';
-const WHITE = 'white';
+export const BLACK = 'black';
+export const WHITE = 'white';
+
+
+export function emptyBoard(size) {
+    if (!isNumber(size) || size <= 0 || size !== parseInt(size)) {
+        throw 'An empty board must be created from a positive integer.';
+    }
+
+    return mori.hashMap('size', size);
+}
+
+
+export function emptySpaces(board) {
+    let size = mori.get(board, 'size');
+}
+
+
+export function addBlackMove(board, position) {
+    // should be legal position per size
+
+    // should be empty
+
+    // should either be outright legal or can kill
+
+    // remove any dead stones if applicable
+
+    return board.assoc(position, BLACK);
+}
 
 
 export default {
-    BLACK, WHITE,
-
-    emptyBoard: function(size) {
-        if (!isNumber(size) || size <= 0 || size !== parseInt(size)) {
-            throw 'An empty board must be created from a positive integer.';
-        }
-
-        return hashMap('size', size);
-    },
-
     addBlackMove: function(board, position) {
+        // TODO
     },
 
     addWhiteMove: function(board, position) {
+        // TODO
     },
 
     addBlackMoves: function(board, positions) {
+        // TODO
     },
 
     addWhiteMoves: function(board, positions) {
+        // TODO
     },
 
     removeMoves: function(board, positions) {
+        // TODO
     },
 
     equivalentBoards: function(board, positions) {
+        // TODO
     },
 
     adjacent: function(board, position) {
+        // TODO
     },
 
     illegalMoves: function(board) {
+        // TODO
     },
 
     isValidPosition: function(size, position) {
+        // TODO
     },
 }
--- test/test_utils.js
+++ test/test_utils.js
@@ -1,29 +1,50 @@
 import assert from 'assert';
 import mori from 'mori';
 
-import goban from '../src/utils';
+import {emptyBoard} from '../src/utils';
 
 
 describe('empty board', function() {
     it('has the size that is passed in and nothing else', function() {
         let size = 19;
-        let board = goban.emptyBoard(19);
+        let board = emptyBoard(19);
 
         assert.ok(mori.hasKey(board, 'size'));
         assert.ok(mori.isEmpty(mori.dissoc(board, 'size')));
+
+        console.log(
+            mori.set(mori.reduce(mori.concat, mori.map(
+                function(t) {return mori.map(mori.vector, mori.last(t),mori.repeat(mori.first(t)))},
+                mori.seq(mori.zipmap(
+                    mori.range(5),
+                    mori.repeat(mori.range(5))
+                ))
+            )))
+        );
+
+        let rg = mori.range(5)
+        console.log(
+            mori.set(mori.reduce(
+                mori.concat,
+                mori.map(
+                    i => mori.map(mori.vector, rg, mori.repeat(i)),
+                    rg
+                )
+            ))
+        );
     });
 
     it('only takes positive integers as size', function() {
         assert.throws(function() {
-            goban.emptyBoard('hi');
+            emptyBoard('hi');
         });
 
         assert.throws(function() {
-            goban.emptyBoard(-5);
+            emptyBoard(-5);
         });
 
         assert.throws(function() {
-            goban.emptyBoard(5.5);
+            emptyBoard(5.5);
         });
     });
 });
