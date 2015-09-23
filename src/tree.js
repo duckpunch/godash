d12096fcefe2e65bb1909fc223fbab06206cd510
Frank Lam <frank@duckpun.ch>
2015-09-23 13:48:31 -0700

    ideas on tree js

---
+++ src/tree.js
@@ -0,0 +1,18 @@
+// how will things get used...
+
+var sgf = loadSgf(url);
+
+// making a reviewable tree requires being able to easily traverse and address nodes of a tree
+
+n <-> n
+  <-> n
+
+
+Map {
+    next: Map {
+        position:
+        move_to_get_here:
+    }
+    position:
+    move_to_get_here:
+}
