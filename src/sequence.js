d8388be212e83cef69f004498fc40d24a4588c9b
Frank Lam <frank@duckpun.ch>
2015-09-24 03:30:05 -0700

    sequence api

--- src/sequence.js
+++ src/sequence.js
@@ -1,14 +1,19 @@
 export class Node {
-    //- id in tree
-    //- move to get here
-    //- current board
-    //- next node(s)
-    //- prev node
-    //- has next
-    //- has lots of next
+    // id in tree
+    // move to get here
+    // current board
+    // next node(s)
+    // prev node
+    // has next
+    // has lots of next
+    // add move, returns tree
 }
 
 
-export class Tree {
-    // Tree.getNodeByPath
+export class Variation {
+    // getNodeByPath
+    // addMove(position, color, path)
+    // getSequence(...path):List
+    // backed by a map, index by path
+    // e.g. [move_number, paths, to, take]
 }
