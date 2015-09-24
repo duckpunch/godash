d672d5e4e325e86da8c29109749c2843261e6ec9
Frank Lam <frank@duckpun.ch>
2015-09-24 02:53:35 -0700

    sequence, with some outline on api

---
+++ src/sequence.js
@@ -0,0 +1,14 @@
+export class Node {
+    //- id in tree
+    //- move to get here
+    //- current board
+    //- next node(s)
+    //- prev node
+    //- has next
+    //- has lots of next
+}
+
+
+export class Tree {
+    // Tree.getNodeByPath
+}
