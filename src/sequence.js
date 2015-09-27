a7570837b5542ca396b3e797502e4db39c791fc7
Frank Lam <frank@duckpun.ch>
2015-09-27 00:42:43 -0700

    more notes on sequence

--- src/sequence.js
+++ src/sequence.js
@@ -2,6 +2,14 @@ import {Map} from 'immutable';
 
 
 function isValidVariationMap(variation) {
+    //MapSchema(
+        //ListSchema(),  // should match variation key sequence - need to extend schema lib to take variable length list
+        //MapSchema(
+            //KeyMatches('board'), isBoard,  // need to add this API to schema lib
+            //KeyMatches('last_move'), isValidPosition,
+            //KeyMatches('meta_stuff'), Something  // not ready for this yet, but should look like this
+        //)
+    //);
     return Map.isMap(variation);
 }
 
