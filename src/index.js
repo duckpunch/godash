65d9ac73d724365206368f2b7baccd83718aaef5
Frank Lam <frank@duckpun.ch>
2016-03-13 17:59:06 -0700

    switching to webpack

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import {BLACK, WHITE} from './analysis';
+import {Board} from './board';
+import {Position, sgfToXY} from './position';
+import {Variation, sgfToVariation} from './sequence';
+
+
+/**
+ * @external {Map} http://facebook.github.io/immutable-js/docs/#/Map
+ */
+
+/**
+ * @external {List} http://facebook.github.io/immutable-js/docs/#/List
+ */
+
+/**
+ * @external {Set} http://facebook.github.io/immutable-js/docs/#/Set
+ */
+
+
+function _Board(...args) {
+    return new Board(...args);
+}
+
+
+_Board.prototype = Board.prototype;
+
+
+function _Variation(...args) {
+    return new Variation(...args);
+}
+
+
+_Variation.prototype = Variation.prototype;
+
+
+export default {
+    Board: _Board,
+    Variation: _Variation,
+    Position,
+    BLACK,
+    WHITE,
+    sgfToXY,
+    sgfToVariation,
+};
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: [
+        './src/index'
+    ],
+    output: {
+        library: 'godash',
+        libraryTarget: 'umd',
+        filename: path.join(__dirname, './dist/godash.js')
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+};
