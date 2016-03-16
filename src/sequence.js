ea6c8254c3999c0ec4dd207e5a08523bd892a63e
Frank Lam <frank@duckpun.ch>
2016-03-15 18:16:23 -0700

    adding comments

--- src/sequence.js
+++ src/sequence.js
@@ -19,7 +19,22 @@ export class Variation {
         if (!boardSize) {
             boardSize = 19;
         }
-        this._variation = Map().set(List.of(0), (new Board(boardSize))._positions);
+        this._variation = Map().set(
+            List.of(0),
+            _makeMoveMap(
+                (new Board(boardSize))._positions,
+                ''
+            )
+        );
+    }
+
+    /**
+     * @private
+     */
+    _makeMoveMap(boardPositions, comments) {
+        return Map()
+            .set('board', boardPositions)
+            .set('comments', comments);
     }
 
     /**
@@ -28,9 +43,25 @@ export class Variation {
      * @param {number} index index to fetch
      */
     getBoardAt(index) {
+        return new Board(this._getDataAt(index, 'board'));
+    }
+
+    /**
+     * Get the comments at provided index.
+     *
+     * @param {number} index index to fetch
+     */
+    getCommentsAt(index) {
+        return this._getDataAt(index, 'comments');
+    }
+
+    /**
+     * @private
+     */
+    _getDataAt(index, dataKey) {
         const key = List.of(index);
         if (this._variation.has(key)) {
-            return new Board(this._variation.get(key));
+            return this._variation.get(key).get(dataKey);
         } else {
             return null;
         }
@@ -55,13 +86,19 @@ export function sgfToVariation(rawSgf) {
     forEach(gameSequence, (move, index) => {
         if (move.B) {
             board = board.addBlackMove(Position(...sgfToXY(move.B)));
-            rawVariation = rawVariation.set(List.of(index + 1), board._positions);
         } else if (move.W) {
             board = board.addWhiteMove(Position(...sgfToXY(move.W)));
-            rawVariation = rawVariation.set(List.of(index + 1), board._positions);
         } else {
             throw Error('Something broken - missing move?');
         }
+
+        rawVariation = rawVariation.set(
+            List.of(index + 1),
+            rawVariation._makeMoveMap(
+                board._positions,
+                move.C || ''
+            )
+        );
     });
 
     variation._variation = rawVariation;
