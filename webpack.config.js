05caa4ebbcbd3d68fbf221ed4735c1b787a1988e
Frank Lam <frank@duckpun.ch>
2018-06-03 00:44:12 -0700

    update to babel env

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel',
                 query: {
-                    presets: ['es2015']
+                    presets: ['env']
                 }
             }
         ]
