f440156dc14f5105516342839be75e8793b1d81b
林鈺錦 <abc1236762@outlook.com>
2020-05-22 18:43:34 +0800

    ts

---
+++ src/sgf.d.ts
@@ -0,0 +1,23 @@
+import { Coordinate } from './board'
+
+type JSr = { [key: string]: string } | JSr[];
+type JS = JSr[];
+type Token = string | [string, string];
+
+export declare const START_MOVE: string;
+export declare const START: string;
+export declare const END: string;
+
+export declare function coordinateToSgfPoint(coordinate: Coordinate): string;
+
+export declare function sgfPointToCoordinate(sgfPoint: string): Coordinate;
+
+export declare function sgfToJS(sgf: string): JS;
+
+export declare function compactMoves(
+  tokens: Token[]
+): (Token | { [key: string]: string })[];
+
+export declare function tokenize(rawSgf: string): Token[];
+
+export declare function nextToken(partialSgf: string): [Token, string];
